3aae176744e6688925ebd31e46f3ac45dd3ca9e5
Matthew Cwalina <matthewcwalina@gmail.com>
2023-03-03 12:26:14 +0100

    Added typewriter effect to banner with css

--- app/javascript/components/banner/banner.jsx
+++ app/javascript/components/banner/banner.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Typewriter from 'typewriter-effect';
+
+
 
 export default function Banner() {
   return (
     <div className="title-container">
-      <h1 className="title">Matthew Cwalina</h1>
-      <h2 className="title-description">Full Stack Developer</h2>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString('<h1 style="font-size: 100px">Matthew Cwalina</h1>')
+            .pauseFor(500)
+            .typeString('<h2 style="font-size: 80px; color: #A5A5A5; justify-content: center; display: flex">Full Stack Developer</h2>')
+            .start();
+        }}
+      />
       <div className='social-icons-container'>
         <a href='https://github.com/m-cwalina'><GitHubIcon className='social-icon' /></a>
         <a href='https://www.linkedin.com/in/matthew-cwalina/'><LinkedInIcon className='social-icon' /></a>
