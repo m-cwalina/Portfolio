2e4c8f185ae1abab6a0d4f442c9e88523f53c5c5
Matthew Cwalina <matthewcwalina@gmail.com>
2023-03-08 08:50:06 +0100

    Setup about page

--- app/javascript/screens/about/about.jsx
+++ app/javascript/screens/about/about.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function About() {
   return (
-    <h1>About Page</h1>
+    <h1>Developing applications from frontend to backend is my obsession.
+        I'm committed to writing CLEANer code and sharp UIs with each iteration.
+        The rewarding feeling of when code compiles and works is an addiction.</h1>
   )
 }
