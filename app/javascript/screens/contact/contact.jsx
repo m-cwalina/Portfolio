cbdc1f6c5fada7154ee7587dc2ba427c7a8eab08
Matthew Cwalina <matthewcwalina@gmail.com>
2023-03-14 17:58:20 +0100

    Created the contact screen

--- app/javascript/screens/contact/contact.jsx
+++ app/javascript/screens/contact/contact.jsx
@@ -1,38 +1,78 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Stack } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import EmailIcon from '../../icons/email'
+import GermanIcon from '../../icons/germannumber'
+import USAIcon from '../../icons/usanumber'
 
 export default function Contact() {
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627
+      lat: 39.187253,
+      lng: -54.24015
     },
-    zoom: 11
+    zoom: 2
   };
 
+  const Marker = ({text}) => (
+    <div>
+      <LocationOnIcon
+        style ={{
+          textAlign: 'center',
+          color: 'red',
+          transform: 'translate(-50%, -50%)',
+          position: 'absolute'}}
+          text={text}
+      >
+      </LocationOnIcon>
+    </div>
+  )
+
   return (
-    <>
-      <Typography align='center' variant='h1' component='div' fontWeight='bold'>
-        Get in Touch
-      </Typography>
-      <Box>
-        <div style={{ height: '100vh', width: '100%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.API_KEY }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-          >
-            <AnyReactComponent
-              lat={59.955413}
-              lng={30.337844}
-              text="My Marker"
-            />
-          </GoogleMapReact>
-        </div>
+    <Box>
+      <Box sx={{ height: '400px', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.API_KEY }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <Marker
+            lat={52.5200}
+            lng={13.4050}
+            text={"Berlin"}
+          />
+          <Marker
+            lat={37.3387}
+            lng={-121.8853}
+            text={"San Jose"}
+          />
+        </GoogleMapReact>
       </Box>
-    </>
+
+      <Typography variant='h2' align='center' fontWeight='bold' color="#5AA7BE" gutterBottom marginTop='50px'>Contact Info</Typography>
+
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Box display='flex' alignItems='flex-start' flexDirection='column' width='50%'>
+          <Stack spacing={2} direction='row' justifyContent="center" alignItems="center">
+            <Typography align='left'><img src={EmailIcon} className='icons'></img></Typography>
+            <Typography variant='h6' align='center' sx={{ fontWeight: 'bold' }} color="#A5A5A5">MatthewCwalina@gmail.com</Typography>
+          </Stack>
+          <Stack spacing={2} direction='row' justifyContent="center" alignItems="center">
+            <Typography align='left'><img src={GermanIcon} className='icons'></img></Typography>
+            <Typography variant='h6' align='center' sx={{ fontWeight: 'bold' }} color="#A5A5A5">(+49) 177-3168291</Typography>
+          </Stack>
+          <Stack spacing={2} direction='row' justifyContent="center" alignItems="center">
+            <Typography align='left'><img src={USAIcon} className='icons'></img></Typography>
+            <Typography variant='h6' align='center' sx={{ fontWeight: 'bold' }} color="#A5A5A5">(+1) 619-9285218</Typography>
+          </Stack>
+        </Box>
+        <Box display='flex' justifyContent='center' width="50%">
+          <Typography variant='h6' align='justify' fontWeight='bold' color="#FFFFFF">
+            Please feel free to reach out anytime. I am currently looking for work and would love to talk about any current opportunities.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   )
 }
