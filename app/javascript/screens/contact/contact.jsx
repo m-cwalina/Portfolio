2f80ad1ef900f2b89a38f16896498d8d669ea749
Matthew Cwalina <matthewcwalina@gmail.com>
2023-03-14 11:12:33 +0100

    Removed wrapper for mapbox

--- app/javascript/screens/contact/contact.jsx
+++ app/javascript/screens/contact/contact.jsx
@@ -1,10 +1,17 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import React from 'react';
+import Map from './map';
 
 export default function Contact() {
+
   return (
-    <Typography align='center' variant='h1' component='div' fontWeight='bold'>
-      Get in Touch
-    </Typography>
+    <>
+      <Typography align='center' variant='h1' component='div' fontWeight='bold'>
+        Get in Touch
+      </Typography>
+      <Box>
+        <Map />
+      </Box>
+    </>
   )
 }
---
+++ app/javascript/screens/contact/map.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWN3YWxpbmEiLCJhIjoiY2xmODFxNmFwMGE1OTN0bzI2cXh1NDhhcCJ9.MftWHNVfXFoGjQj3bW9VPA';
+
+
+export default function Map() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
