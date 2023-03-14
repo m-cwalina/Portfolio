12661eb1ff2183ee405177172b5039f0669181bb
Matthew Cwalina <matthewcwalina@gmail.com>
2023-03-14 18:15:08 +0100

    Moved dotenv into production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const Dotenv = require('dotenv-webpack');
+environment.plugins.prepend('Dotenv', new Dotenv());
 
 const environment = require('./environment')
 
