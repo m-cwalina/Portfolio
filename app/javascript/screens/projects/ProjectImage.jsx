d8edf97e5c354ed852edd660788c5abbc4717012
Matthew Cwalina <matthewcwalina@gmail.com>
2023-05-23 15:15:32 +0200

    Centered circle

--- app/javascript/screens/projects/ProjectImage.jsx
+++ app/javascript/screens/projects/ProjectImage.jsx
@@ -31,35 +31,10 @@ export default function ProjectImage({ project }) {
   return (
     <>
       {isLoading ? (
-        <CircularProgress size={100} />
+        <CircularProgress size={100} style={{ color: '#164e63' }} />
       ) : (
         <img src={getImageSrc(project.image)} className='images' alt={project.image} />
       )}
     </>
   );
 }
-
-// import React from 'react';
-// import PortfolioPic from '../../images/portfolio.jpg';
-// import ConnectGolfPic from '../../images/connectgolf.jpg';
-// import PfandMePic from '../../images/pfandme.jpg';
-// import ChartsPic from '../../images/charts.jpg';
-// import ForteFans from '../../images/fortefans.png';
-
-// export default function Image(project) {
-
-//   const Image = ({ image }) => {
-//     switch (image) {
-//       case 'ConnectGolf': return <img src={ConnectGolfPic} className='images'></img>
-//       case 'PfandMe': return <img src={PfandMePic} className='images'></img>
-//       case 'Portfolio': return <img src={PortfolioPic} className='images'></img>
-//       case 'Charts': return <img src={ChartsPic} className='images'></img>
-//       case 'ForteFans': return <img src={ForteFans} className='images'></img>
-//       default: return null
-//     }
-//   }
-
-//   return (
-//     <Image image={project.image} />
-//   )
-// }
